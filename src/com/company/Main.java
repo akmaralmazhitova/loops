daa90e42620d7f2c9e99c05a4d29dbb430032905
akmaralmazhitova <akmaral.mazhitova.kk@gmail.com>
2022-02-01 21:07:37 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            int a = 7;
+            while (a<=98){
+                System.out.print(a+" ");
+                a+=7;
+            }
+
+        }
+}
